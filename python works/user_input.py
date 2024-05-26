3368b4962007e1c61742154ecb22449810f8b75a
obikiel <ezekielobiomachi1@gmail.com>
2024-05-26 14:30:14 +0100

    task submission

---
+++ python works/user_input.py
@@ -0,0 +1,5 @@
+name = input("please what is your name: ")
+age = input("i would like to know your age: ")
+location = input("one last thing, where is your location: ")
+
+print("your name is " + name + " ,you are " + age + " years old" + " and you are located in " + location)
